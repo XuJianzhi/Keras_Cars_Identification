10af33be583cafa8497671d0d2d8636eceedcb7e
XuJianzhi <30584453+XuJianzhi@users.noreply.github.com>
2018-01-19 16:13:49 +0800

    Add files via upload

---
+++ car_pred.py
@@ -0,0 +1,207 @@
+
+
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Conv2D, Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.preprocessing.image import load_img, img_to_array
+from keras.layers import Input
+
+import cv2, os, numpy as np, pandas as pd, time
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
+x_all = []
+y_all = []
+
+path_in = '/home/m/桌面/cars/备份/1/'
+list_1 = ['jili','baoma','benchi','jeep','qirui']
+for car_name in list_1:
+	list_2 = os.listdir(path_in + car_name)
+	for img_name in tqdm(list_2[:200]):
+		img = load_img(path_in + car_name + '/' + img_name, target_size=(70,70))
+		#img = img_to_array(img).flatten() / 255
+		img = img_to_array(img) / 255
+		img = [img.tolist()]
+		x_all += img
+		y_all += [car_name]
+
+#print(np.array(x_all).shape)
+#print(np.array(y_all).shape)
+
+	
+
+x_train, x_test, y_train, y_test = train_test_split(np.array(x_all), np.array(y_all))
+
+classes = 5		# 几种车
+
+encoder = LabelEncoder()
+hotencoder = OneHotEncoder(sparse=False)
+y_train = hotencoder.fit_transform(pd.DataFrame({'a':encoder.fit_transform(y_train)}))
+y_test = hotencoder.transform(pd.DataFrame({'a':encoder.transform(y_test)}))
+
+
+model = Sequential()
+#model.add(Dense(input_dim=100*100,units=633,activation='relu'))
+#model.add(Conv2D(filters= 32, kernel_size=(5,5), padding='Same', activation='relu',input_shape=(28,28,1)))
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', input_shape=x_train.shape[1:]))	#(100,100,3), data_format='channels_last'))		#input_dim=100*100)),
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+
+model.add(Flatten(name='flatten'))
+model.add(Dense(4096, activation='relu', name='fc1'))
+model.add(Dense(4096, activation='relu', name='fc2'))
+model.add(Dense(classes, activation='softmax', name='predictions'))
+
+model.compile(loss='mse', optimizer=SGD(lr=0.1), metrics=['accuracy'])	
+model.fit(x_train, y_train, batch_size=100, epochs=50)
+
+result = model.evaluate(x_test, y_test)
+print(result)
+
+
+'''
+def create_vgg16():	
+	
+	input_shape = (100*100)
+	img_input = Input(shape=input_shape)
+	
+	# Block 1	
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    #if include_top:
+    # Classification block
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(classes, activation='softmax', name='predictions')(x)
+	
+	return x
+	
+	
+	
+	
+	
+	
+	
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1000, activation='softmax'))
+
+
+
+
+#vgg = keras.applications.vgg16.VGG16()
+
+#img = cv2.imread(
+path = '/home/m/桌面/cars/baoma/u607940199930981fm27gp0.jpg'
+img = image.load_img(path, target_size=(224, 224))
+
+aaa = img_to_array(img).flatten() / 255
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ car_pred_2.py
@@ -0,0 +1,124 @@
+
+
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Conv2D, Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.preprocessing.image import load_img, img_to_array
+from keras.layers import Input
+
+import keras, cv2, os, numpy as np, pandas as pd, time
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
+x_all = []
+y_all = []
+
+path_in = '/home/m/桌面/cars/'
+list_1 = ['jili','baoma','mumaren']
+for car_name in list_1:
+	list_2 = os.listdir(path_in + car_name)
+	for img_name in tqdm(list_2[:400]):
+		img = load_img(path_in + car_name + '/' + img_name, target_size=(64,64))
+		#img = img_to_array(img).flatten() / 255
+		img = img_to_array(img) / 255
+		img = [img.tolist()]
+		x_all += img
+		y_all += [car_name]
+
+#print(np.array(x_all).shape)
+#print(np.array(y_all).shape)
+
+	
+
+x_train, x_test, y_train, y_test = train_test_split(np.array(x_all), np.array(y_all))
+del x_all, y_all
+
+classes = len(list_1)		# 几种车
+
+encoder = LabelEncoder()
+hotencoder = OneHotEncoder(sparse=False)
+y_train = hotencoder.fit_transform(pd.DataFrame({'a':encoder.fit_transform(y_train)}))
+y_test = hotencoder.transform(pd.DataFrame({'a':encoder.transform(y_test)}))
+
+
+model = Sequential()
+#model.add(Dense(input_dim=100*100,units=633,activation='relu'))
+#model.add(Conv2D(filters= 32, kernel_size=(5,5), padding='Same', activation='relu',input_shape=(28,28,1)))
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', input_shape=x_train.shape[1:]))	#(100,100,3), data_format='channels_last'))		#input_dim=100*100)),
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+'''
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+'''
+model.add(Flatten(name='flatten'))
+model.add(Dense(4096, activation='relu', name='fc1'))
+model.add(Dense(4096, activation='relu', name='fc2'))
+model.add(Dense(classes, activation='softmax', name='predictions'))
+
+#model.compile(loss='mse', optimizer=SGD(lr=0.1), metrics=['accuracy'])	
+
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=50, epochs=50)
+
+result = model.evaluate(x_test, y_test)
+print(result)
+
+
+
+
+
+
+###########
+path_in = '/home/m/桌面/cars/'
+list_1 = ['jili','baoma','mumaren']
+for car_name in list_1:
+	list_2 = os.listdir(path_in + car_name)
+	for img_name in tqdm(list_2[400:500]):
+		img = load_img(path_in + car_name + '/' + img_name, target_size=(70,70))
+		#img = img_to_array(img).flatten() / 255
+		img = img_to_array(img) / 255
+		img = [img.tolist()]
+		x_all += img
+		y_all += [car_name]
+
+x_test = np.array(x_all)
+y_test = np.array(y_all)
+
+y_test = hotencoder.transform(pd.DataFrame({'a':encoder.transform(y_test)}))
+
+y_pred = model.predict(x_test)
+y_pred[y_pred<1.0] = 0
+
+accu = 1 - (len(y_pred)*classes - sum((y_pred == y_test).flatten()))/(2*len(y_pred))
+
+
+
+
+
+
+
